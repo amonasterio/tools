33b7a6fc5dc351db11bf9b73fe5554c5219a57fd
Alejandro Monasterio <amonasterio@gmail.com>
2022-08-31 10:13:20 +0200

    subida inicial

---
+++ app.py
@@ -0,0 +1,101 @@
+from math import comb
+from xml import dom
+import streamlit as st
+import sistrix_functions as sf
+import pandas as pd
+from functools import reduce
+
+
+#lista de dataframes a combinar indicando el nombre de la columna que se tiene como referenca
+def combinarDataFrames(l_df,column_name):
+    columnas=['date']
+    for df in l_df:
+        dominio=df['domain'].iloc[0]
+        pais=df['country'].iloc[0]
+        device=df['dispositivo'].iloc[0]
+        nombre=dominio+"_"+pais+"_"+device
+        columnas.append(nombre)
+    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=[column_name],
+                                            how='outer'), l_df).fillna('0').reset_index(drop=True)
+    #Seleccionamos las columnas que nos interesan
+    deja_columnas=[2]
+    i=1
+    while i<=len(l_df):
+        deja_columnas.append(4*i-1)
+        i+=1
+    df_salida=df_merged.iloc[:,deja_columnas]
+    df_salida.columns=columnas
+    return df_salida
+
+@st.cache
+def getVisibilidadSemanal(df,num_semanas,api_key):
+    l_df=[]
+    for index in range(len(df)):
+        dominio=df['domain'].iloc[index]
+        tipo_consulta=df['type'].iloc[index]
+        pais=df['country'].iloc[index]
+        device=df['device'].iloc[index]
+        if device=='mobile':
+            es_mobile=True
+        else:
+            es_mobile=False
+        l_df.append(sf.getUltimasSemanas(num_semanas,dominio,pais,es_mobile,tipo_consulta,api_key))
+    df_merged = combinarDataFrames(l_df,'date')              
+    return df_merged
+
+@st.cache
+def getVisibilidadDiaria(df,num_dias,api_key):
+    l_df=[]
+    for index in range(len(df)):
+        dominio=df['domain'].iloc[index]
+        tipo_consulta=df['type'].iloc[index]
+        pais=df['country'].iloc[index]
+        device=df['device'].iloc[index]
+        if device=='mobile':
+            es_mobile=True
+        else:
+            es_mobile=False
+        l_df.append(sf.getUltimosDias(num_dias,dominio,pais,es_mobile,tipo_consulta,api_key))
+    df_merged = combinarDataFrames(l_df,'date')              
+    return df_merged
+
+st.set_page_config(
+   page_title="Resultados Sistrix",
+   layout="wide"
+)
+
+st.title("Resultados Sistrix")
+api_key= st.text_input('API key de Sistrix', '')
+if api_key!='':
+    st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
+dominios_analizar=st.file_uploader('CSV con dominios a analizar', type='csv')
+if dominios_analizar is not None:
+    df_entrada=pd.read_csv(dominios_analizar)
+    st.write(df_entrada)
+    
+    num_semanas=st.slider('Últimas semanas a obtener', value=5, min_value=1, max_value=100)   
+
+    btn_sem = st.button('Obtener visibilidad semanal', disabled=False, key='1')
+    if btn_sem:
+        df_salida=getVisibilidadSemanal(df_entrada,num_semanas,api_key)
+        st.write(df_salida)
+        st.download_button(
+        label="Descargar como CSV",
+        data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
+        file_name='visibilidad_semanal.csv',
+        mime='text/csv'
+        )
+        st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
+   
+    num_dias=st.slider('Últimos días a obtener', value=32, min_value=1, max_value=90)
+    btn_d = st.button('Obtener visibilidad diaria', disabled=False, key='3')
+    if btn_d:
+        df_salida=getVisibilidadDiaria(df_entrada,num_dias,api_key)
+        st.write(df_salida)
+        st.download_button(
+        label="Descargar como CSV",
+        data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
+        file_name='visibilidad_diaria.csv',
+        mime='text/csv'
+        )
+        st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
---
+++ sistrix_functions.py
@@ -0,0 +1,160 @@
+import requests, pandas as pd
+from xml.etree import ElementTree as ET
+from datetime import  datetime
+
+
+
+def convierteFechaSistrix(fecha):
+    #dejamos los 10 primeros caracteres
+    date=fecha[0:10]
+    formato_entrada='%Y-%m-%d'
+    formato_salida='%d-%m-%Y'
+    date_time_obj = datetime.strptime(date,formato_entrada)
+    return date_time_obj.strftime(formato_salida)
+
+def getDataFrameResponse(url,es_mobile,es_host):
+    response=requests.get(url)
+    dct_arr=[]
+    df=None
+    if response.status_code==200:
+        root=ET.fromstring(response.content)
+    
+        for child in root.findall('.//sichtbarkeitsindex'):
+            dict={}
+            if not es_host:
+                dict['domain']=child.attrib['domain']
+            else:
+                dict['domain']=child.attrib['host']
+            if es_mobile:
+                dict['dispositivo']='mobile'
+            else:
+                dict['dispositivo']='desktop'
+            dict['date']=convierteFechaSistrix(child.attrib['date'])
+            if child.attrib['value']!=None:
+                dict['value']=str(child.attrib['value']).replace('.',',')
+            else:
+                if len(dct_arr)>1:
+                    dict['value']=dct_arr[len(dct_arr)-1].get('value')
+                else:
+                    dict['value']=''
+            dct_arr.append(dict)
+        dct_arr.reverse()
+        df = pd.DataFrame(dct_arr)
+    return df  
+
+def getCreditsResponse(url):
+    credits=-1
+    response=requests.get(url)
+    if response.status_code==200:
+        root=ET.fromstring(response.content)
+        for child in root.findall('.//credits'):
+            if child.attrib['value']!=None:
+                credits=str(child.attrib['value'])
+                break
+    return credits   
+
+def getCredits(api_key):
+    url='https://api.sistrix.com/credits?api_key='+api_key
+    credits=getCreditsResponse(url)
+    return credits
+
+
+def getUltimasSemanas(num_semanas, dominio, pais, es_mobile,es_host, api_key):
+    url='https://api.sistrix.com/domain.sichtbarkeitsindex?api_key='+api_key
+    if not es_host:
+        url+='&domain='+dominio
+    else:
+        url+='&host='+dominio
+    url+='&country='+pais+'&history=true&num='+str(num_semanas)
+    if es_mobile:
+        url+='&mobile=true'
+    df=getDataFrameResponse(url,es_mobile,es_host)
+    return df
+
+def getUltimosDias(num_dias, dominio, pais, es_mobile,es_host, api_key):
+    url='https://api.sistrix.com/domain.sichtbarkeitsindex?api_key='+api_key
+    if not es_host:
+        url+='&domain='+dominio
+    else:
+        url+='&host='+dominio
+    url+='&country='+pais+'&history=true&num='+str(num_dias)
+    if es_mobile:
+        url+='&mobile=true'
+    url+='&daily=true'
+    df=getDataFrameResponse(url,es_mobile,es_host)
+    return df
+
+def getUltimasSemanas(num_semanas, dominio, pais, es_mobile,es_host, api_key):
+    url='https://api.sistrix.com/domain.sichtbarkeitsindex?api_key='+api_key
+    if not es_host:
+        url+='&domain='+dominio
+    else:
+        url+='&host='+dominio
+    url+='&country='+pais+'&history=true&num='+str(num_semanas)
+    if es_mobile:
+        url+='&mobile=true'
+    df=getDataFrameResponse(url,es_mobile,es_host)
+    return df
+
+
+def getDataFrameResponse(url,es_mobile,tipo_consulta,pais):
+    response=requests.get(url)
+    dct_arr=[]
+    df=None
+    if response.status_code==200:
+        root=ET.fromstring(response.content)
+    
+        for child in root.findall('.//sichtbarkeitsindex'):
+            dict={}
+            if tipo_consulta =='domain':
+                dict['domain']=child.attrib['domain']
+            elif tipo_consulta =='host':
+                dict['domain']=child.attrib['host']
+            elif tipo_consulta =='path':
+                dict['domain']=child.attrib['path']
+            if es_mobile:
+                dict['dispositivo']='mobile'
+            else:
+                dict['dispositivo']='desktop'
+            dict['date']=convierteFechaSistrix(child.attrib['date'])
+            if child.attrib['value']!=None:
+                dict['value']=str(child.attrib['value']).replace('.',',')
+            else:
+                if len(dct_arr)>1:
+                    dict['value']=dct_arr[len(dct_arr)-1].get('value')
+                else:
+                    dict['value']=''
+            dict['country']=pais
+            dct_arr.append(dict)
+        dct_arr.reverse()
+        df = pd.DataFrame(dct_arr)
+    return df  
+
+def getUltimosDias(num_dias, dominio, pais, es_mobile,tipo_consulta, api_key):
+    url='https://api.sistrix.com/domain.sichtbarkeitsindex?api_key='+api_key
+    if tipo_consulta =='domain':
+        url+='&domain='+dominio
+    elif tipo_consulta=='host':
+        url+='&host='+dominio
+    elif tipo_consulta=='path':
+        url+='&path='+dominio
+    url+='&country='+pais+'&history=true&num='+str(num_dias)
+    if es_mobile:
+        url+='&mobile=true'
+    url+='&daily=true'
+    df=getDataFrameResponse(url,es_mobile,tipo_consulta,pais)
+    return df
+
+def getUltimasSemanas(num_semanas, dominio, pais, es_mobile,tipo_consulta, api_key):
+    url='https://api.sistrix.com/domain.sichtbarkeitsindex?api_key='+api_key
+    if tipo_consulta =='domain':
+        url+='&domain='+dominio
+    elif tipo_consulta =='host':
+        url+='&host='+dominio
+    elif tipo_consulta=='path':
+        url+='&path='+dominio
+    url+='&country='+pais+'&history=true&num='+str(num_semanas)
+    if es_mobile:
+        url+='&mobile=true'
+    df=getDataFrameResponse(url,es_mobile,tipo_consulta,pais)
+    return df
