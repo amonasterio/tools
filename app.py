860e59e5ef71cedaf5cb2e540fcc6193442f3413
Alejandro Monasterio <amonasterio@gmail.com>
2022-08-31 13:24:59 +0200

    modifico para que no deje continuar si hay pocos créditos

--- app.py
+++ app.py
@@ -70,35 +70,39 @@ st.set_page_config(
 st.title("Resultados Sistrix")
 api_key= st.text_input('API key de Sistrix', '')
 if api_key!='':
-    st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
-dominios_analizar=st.file_uploader('CSV con dominios a analizar', type='csv')
-if dominios_analizar is not None:
-    df_entrada=pd.read_csv(dominios_analizar)
-    st.write(df_entrada)
-    
-    num_semanas=st.slider('Últimas semanas a obtener', value=5, min_value=1, max_value=100)   
+    creditos=sf.getCredits(api_key)
+    st.write('Créditos semanales restantes: '+creditos)
+    if int(creditos) > 1000:
+        dominios_analizar=st.file_uploader('CSV con dominios a analizar', type='csv')
+        if dominios_analizar is not None:
+            df_entrada=pd.read_csv(dominios_analizar)
+            st.write(df_entrada)
+            
+            num_semanas=st.slider('Últimas semanas a obtener', value=5, min_value=1, max_value=100)   
 
-    btn_sem = st.button('Obtener visibilidad semanal', disabled=False, key='1')
-    if btn_sem:
-        df_salida=getVisibilidadSemanal(df_entrada,num_semanas,api_key)
-        st.write(df_salida)
-        st.download_button(
-        label="Descargar como CSV",
-        data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
-        file_name='visibilidad_semanal.csv',
-        mime='text/csv'
-        )
-        st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
-   
-    num_dias=st.slider('Últimos días a obtener', value=32, min_value=1, max_value=90)
-    btn_d = st.button('Obtener visibilidad diaria', disabled=False, key='3')
-    if btn_d:
-        df_salida=getVisibilidadDiaria(df_entrada,num_dias,api_key)
-        st.write(df_salida)
-        st.download_button(
-        label="Descargar como CSV",
-        data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
-        file_name='visibilidad_diaria.csv',
-        mime='text/csv'
-        )
-        st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
+            btn_sem = st.button('Obtener visibilidad semanal', disabled=False, key='1')
+            if btn_sem:
+                df_salida=getVisibilidadSemanal(df_entrada,num_semanas,api_key)
+                st.write(df_salida)
+                st.download_button(
+                label="Descargar como CSV",
+                data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
+                file_name='visibilidad_semanal.csv',
+                mime='text/csv'
+                )
+                st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
+        
+            num_dias=st.slider('Últimos días a obtener', value=32, min_value=1, max_value=90)
+            btn_d = st.button('Obtener visibilidad diaria', disabled=False, key='3')
+            if btn_d:
+                df_salida=getVisibilidadDiaria(df_entrada,num_dias,api_key)
+                st.write(df_salida)
+                st.download_button(
+                label="Descargar como CSV",
+                data=df_salida.to_csv(index=False, sep=",").encode('utf-8'),
+                file_name='visibilidad_diaria.csv',
+                mime='text/csv'
+                )
+                st.write('Créditos semanales restantes: '+sf.getCredits(api_key))
+    else:
+        st.write('Volumen de créditos bajo. No usar hasta tener más')
